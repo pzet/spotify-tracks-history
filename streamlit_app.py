bfafcab1f2bec7f8af3f7f01682b1c76a82bc17e
Piotrek Zawal <piotr.zawal@gmail.com>
2021-10-25 15:29:43 +0100

    copied script from tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import psycopg2
 
+# Initialize connection.
+# Uses st.cache to only run once.
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
 
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * FROM recent_tracks")
+rows = run_query("SELECT * from recent_tracks;")
 
+# Print results.
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
