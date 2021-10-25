f64ba9b04dee0407ac7914d8db08735ebd6d1311
Piotrek Zawal <piotr.zawal@gmail.com>
2021-10-25 13:53:09 +0100

    added query function

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ import psycopg2
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM recent_tracks")
