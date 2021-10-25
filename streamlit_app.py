5f2a6012dad98525f03a74219935a6b8c9b54849
Piotrek Zawal <piotr.zawal@gmail.com>
2021-10-25 13:48:12 +0100

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import psycopg2
+
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
