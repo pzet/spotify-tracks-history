9a12099638a0f2ca8ef5a32cc81510e47cfe7187
Piotrek Zawal <piotr.zawal@gmail.com>
2021-10-25 15:37:10 +0100

    added builtins.weakref hash func

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import psycopg2
 
 # Initialize connection.
 # Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+@st.cache(allow_output_mutation=True, hash_funcs={
+                                                "_thread.RLock": lambda _: None,
+                                                "builtins.weakref": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
