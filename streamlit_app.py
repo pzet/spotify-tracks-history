47c8692288840a3eeaae83a2ae26207d384ed376
Piotrek Zawal <piotr.zawal@gmail.com>
2021-10-25 16:31:51 +0100

    removed caching

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import psycopg2
 
 # Initialize connection.
 # Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={
-                                                "_thread.RLock": lambda _: None})
+# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
